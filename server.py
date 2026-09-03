c31061204fba9c748821b7a1d4a3243e9a94e510
hasanvekoc <hasanvekoc@gmail.com>
2026-09-03 22:08:14 +0300

    server.py

--- server.py
+++ server.py
@@ -70,7 +70,7 @@ def block_row(height):
     r = ledger.db.execute(
         'SELECT height,hash,prev_hash,proposer,payload,state_root FROM blocks WHERE height=?',
         (height,)
-    except Exception: pa…
+    except Exception: pass 
     if not r:
         return None
     return {
